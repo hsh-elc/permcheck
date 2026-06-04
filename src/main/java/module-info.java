06a9979ccb3870c09c0ca3cf80fc2d9d6bab6284
Robert Garmann <robert.garmann@hs-hannover.de>
2026-06-04 19:19:14 +0200

    WIP

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module de.hsh.permcheck {
+    requires net.bytebuddy;
+    requires net.bytebuddy.agent;
+    requires java.desktop;
+    requires java.compiler;
+    requires java.instrument;
+
+    exports de.hsh.permcheck;
+}
